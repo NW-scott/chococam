67afe40ba1591eb2d168bb66ab458f7cab9ada90
NW-scott <scott.ndjofang@facsciencs-uy1.cm>
2024-11-16 17:24:20 +0100

    version 1

---
+++ school.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int f;
+    printf("i like bomboms\n");
+    printf(" how many time do you bath : \n");
+    scanf("%d",&f);
+    printf(" the number of times you bath is : %d\n", f);
+    return 0;
+}
